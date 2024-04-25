b7643c30f27a7154c94856176ecbd3bc9c500272
Kingkor Roy Tirtho <krtirtho@gmail.com>
2024-04-25 12:16:58 +0600

    feat: windows named pipe support

--- lib/src/ipc/windows.dart
+++ lib/src/ipc/windows.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+import 'dart:convert';
 import 'dart:ffi';
+import 'dart:typed_data';
 
 import 'package:ffi/ffi.dart';
 import 'package:win32/win32.dart';
@@ -7,6 +10,7 @@ import 'package:discord_rich_presence/src/ipc/ipc.dart';
 
 class WindowsIPC extends UniversalIPC {
   int? _handle;
+  final _streamController = StreamController<String>();
 
   @override
   Future<void> closeIpc() async {
@@ -46,17 +50,57 @@ class WindowsIPC extends UniversalIPC {
     if (_handle == null) {
       throw WindowsException(HRESULT_FROM_WIN32(GetLastError()));
     }
+
+    // non blocking read file loop. We can use unpack to know the size of the next message
+    // and then read that amount of bytes
+    await Future(() async {
+      while (true) {
+        final data = calloc<Uint8>(4096);
+        final read = calloc<Uint32>();
+
+        final success = ReadFile(
+          _handle!,
+          data,
+          8,
+          read,
+          nullptr,
+        );
+
+        if (success == 0) {
+          throw WindowsException(HRESULT_FROM_WIN32(GetLastError()));
+        }
+
+        final size = data.asTypedList(8).buffer.asByteData().getUint64(0);
+
+        calloc.free(data);
+        calloc.free(read);
+
+        final data2 = calloc<Uint8>(size);
+        final read2 = calloc<Uint32>();
+
+        final success2 = ReadFile(
+          _handle!,
+          data2,
+          size,
+          read2,
+          nullptr,
+        );
+
+        if (success2 == 0) {
+          throw WindowsException(HRESULT_FROM_WIN32(GetLastError()));
+        }
+
+        final json = utf8.decode(data2.asTypedList(size));
+
+        _streamController.add(json);
+
+        calloc.free(data2);
+        calloc.free(read2);
+      }
+    });
   }
 
-  @override
-  Future<void> send(String json, int opcode) async {
-    if (_handle == null) {
-      throw Exception('IPC not connected');
-    }
-
-    final data = pack(opcode, json.length);
-    final written = calloc<Uint32>();
-
+  void write(Uint8List data) {
     final dataPtr = VirtualAllocEx(
       GetCurrentProcess(),
       nullptr,
@@ -66,6 +110,8 @@ class WindowsIPC extends UniversalIPC {
     ).cast<Uint8>();
 
     dataPtr.asTypedList(data.lengthInBytes).setAll(0, data);
+
+    final written = calloc<Uint32>();
 
     final success = WriteFile(
       _handle!,
@@ -89,6 +135,17 @@ class WindowsIPC extends UniversalIPC {
   }
 
   @override
-  // TODO: implement stream
-  Stream<String> get stream => throw UnimplementedError();
+  Future<void> send(String json, int opcode) async {
+    if (_handle == null) {
+      throw Exception('IPC not connected');
+    }
+
+    final data = pack(opcode, json.length);
+
+    write(data);
+    write(utf8.encode(json));
+  }
+
+  @override
+  Stream<String> get stream => _streamController.stream;
 }
